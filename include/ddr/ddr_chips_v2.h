afc68545b37ba674cbee4afb8ec1da0fbff009b0
zhuzhenbin <zhuzhenbin@cviauo.com>
2023-12-16 04:37:43 +0000

    Init commit.

---
+++ include/ddr/ddr_chips_v2.h
@@ -0,0 +1,14 @@
+#ifndef __DDR_CHIPS_V2_H__
+#define __DDR_CHIPS_V2_H__
+
+#include  "chips-v2/ddr_chip.h"
+
+#include "chips-v2/LPDDR3_W63AH6NKB-BI.h"
+#include "chips-v2/LPDDR2_W97BV6MK.h"
+#include "chips-v2/LPDDR2_M54D5121632A.h"
+#include "chips-v2/LPDDR2_SCB4BL256160AFL19GI.h"
+#include "chips-v2/DDR3_W631GU6NG.h"
+#include "chips-v2/LPDDR3_NK6CL256M16DKX-H1.h"
+#include "chips-v2/DDR3_NK5CC128M8HKX.h"
+
+#endif
